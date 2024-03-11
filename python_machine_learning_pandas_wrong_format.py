50ab5f56659e1884dc7a1ad6345f1f4a888c017d
Otabek Razzoqov <110417699+otabekrazzoqov@users.noreply.github.com>
2024-03-11 20:41:07 +0900

    Update python_machine_learning_pandas_wrong_format.py

--- python_machine_learning_pandas_wrong_format.py
+++ python_machine_learning_pandas_wrong_format.py
@@ -61,4 +61,43 @@ Duration          Date  Pulse  Maxpulse  Calories
 """
 
 
+
+# Let's try to convert all cells in the 'Date' column into dates.
+# Pandas has a to_datetime() method for this:
     
+    
+import pandas as pd
+df = pd.read_csv('datad.csv')
+print(df.to_string())
+#df['Date'] = pd.to_datetime(df['Date'])
+#print(df.to_string())   
+
+
+
+"""
+
+As you can see from the result, the date in row 26 was fixed, 
+but the empty date in row 22 got a NaT (Not a Time) value, 
+in other words an empty value. One way to deal with empty 
+values is simply removing the entire row.
+
+"""
+
+
+#              Removing Rows
+
+# The result from the converting in the example above gave us a NaT value, 
+# which can be handled as a NULL value, and we can remove 
+# the row by using the dropna() method.
+
+# Example
+# Remove rows with a NULL value in the "Date" column:
+
+df.dropna(subset=['Date'], inplace = True)
+print(df.to_string())
+
+
+
+
+
+ 
